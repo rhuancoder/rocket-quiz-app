85118bd4abe7c56524f4412e9e02a7fb59bbfa01
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-02-16 20:59:21 -0300

    refactor: change button layout

--- RocketQuiz/ViewController.swift
+++ RocketQuiz/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startQuizButton: UIButton!
+    
+    @IBAction func pressedButton(_ sender: Any) {
+        print("The button has been pressed!")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
 
-
+    func setupView() {
+        startQuizButton.layer.cornerRadius = 12.0
+    }
 }
 
