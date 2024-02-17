7d87555d52576a7cfe69f6dffb96509b86307f46
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-02-17 19:22:04 -0300

    feat: add question view

---
+++ RocketQuiz/QuestionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionViewController.swift
+//  RocketQuiz
+//
+//  Created by Rhuan Carvalho on 17/02/24.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
