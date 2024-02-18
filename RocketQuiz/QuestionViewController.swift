a2bf23c542a7a2ac5d77d11e50c6476eb1864664
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-02-18 19:47:48 -0300

    feat: add question title

--- RocketQuiz/QuestionViewController.swift
+++ RocketQuiz/QuestionViewController.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-
+    var score = 0
+    var questionNumber = 0
+    
+    @IBOutlet weak var questionTitleLabel: UILabel!
+    @IBOutlet var answerButtons: [UIButton]!
+    
+    @IBAction func answerPressedButton(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationItem.hidesBackButton = true
+        setupLayout()
+        setQuestion()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupLayout() {
+        navigationItem.hidesBackButton = true
+        questionTitleLabel.numberOfLines = 0
+        questionTitleLabel.textAlignment = .center
+        
+        for answerButton in answerButtons {
+            answerButton.layer.cornerRadius = 12.0
+        }
     }
-    */
-
+    
+    func setQuestion() {
+        questionTitleLabel.text = questions[questionNumber].title
+    }
 }
